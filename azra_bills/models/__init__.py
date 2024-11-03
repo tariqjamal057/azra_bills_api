233f90de278d01f78657e5c51a3c4a174e615754
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-11-04 01:57:02 +0530

    feat: add store model

--- azra_bills/models/__init__.py
+++ azra_bills/models/__init__.py
@@ -1,3 +1,5 @@
-from azra_bills.models.country import Country
-from azra_bills.models.state import State
-from azra_bills.models.city import City
+from azra_bills.models.city import City as City
+from azra_bills.models.country import Country as Country
+from azra_bills.models.state import State as State
+from azra_bills.models.store import Store as Store
+from azra_bills.models.user import User as User
