ddfdfb6329ab5a5bb6e505120de02677ff6efd92
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-11-14 00:11:24 +0530

    feat: add logger configuration and add docs in utils module

---
+++ core/constant.py
@@ -0,0 +1,4 @@
+"""This module contains package constants."""
+
+EMAIL_PATTERN = r"^[a-zA-Z0-9_-.+]@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+PHONE_NUMBER_PATTERN = r"^\d{10}$"
--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,6 @@
 import os
+import random
+import string
 import time
 import uuid
 from enum import Enum
@@ -9,12 +11,24 @@ def uuid_generator():
 
 
 class BaseEnum(Enum):
+    """A base enumeration class that provides additional utility methods."""
+
     @property
     def name(self):
+        """Returns the name of the enum member with underscores replaced by spaces and title-
+        cased."""
         return self._name_.replace("_", " ").title()
 
     @classmethod
     def get_name_by_value(cls: type[Enum], value):
+        """Returns the name of the enum member with the given value.
+
+        Args:
+            value: The value to search for.
+
+        Returns:
+            str: The name of the enum member if found, None otherwise.
+        """
         for member in cls:
             if member.value == value:
                 return member.name
@@ -22,14 +36,29 @@ class BaseEnum(Enum):
 
     @classmethod
     def all(cls: type[Enum]):
+        """Returns a list of dictionaries containing all enum members.
+
+        Returns:
+            list: A list of dictionaries with 'id' (value) and 'name' for each enum member.
+        """
         return [{"id": member.value, "name": member.name} for member in cls]
 
     @classmethod
     def all_ids(cls):
+        """Returns a list of all enum member values.
+
+        Returns:
+            list: A list of all enum member values.
+        """
         return [member.value for member in cls]
 
     @classmethod
     def all_names(cls):
+        """Returns a list of all enum member names.
+
+        Returns:
+            list: A list of all enum member names.
+        """
         return [member.name for member in cls]
 
 
@@ -63,3 +92,33 @@ class ULIDGenerator:
 
         # Combine the two parts to form the ULID
         return timestamp_encoded + randomness_encoded
+
+
+def generate_password(n: int = 8) -> str:
+    """Generate a random password with specified length and character types.
+
+    This function creates a password that includes at least one uppercase letter,
+    one lowercase letter, one digit, and one special character. The remaining
+    characters are randomly chosen from all character types.
+
+    Args:
+        n (int): The length of the password to generate. Defaults to 8.
+
+    Returns:
+        str: A randomly generated password of the specified length.
+    """
+
+    uppercase_chars = string.ascii_uppercase
+    lowercase_chars = string.ascii_lowercase
+    digits = string.digits
+    special_chars = string.punctuation
+    random_string = [
+        random.choice(uppercase_chars),
+        random.choice(lowercase_chars),
+        random.choice(digits),
+        random.choice(special_chars),
+    ]
+    all_chars = uppercase_chars + lowercase_chars + digits + special_chars
+    random_string += [random.choice(all_chars) for _ in range(n - 4)]
+    random.shuffle(random_string)
+    return "".join(random_string)
