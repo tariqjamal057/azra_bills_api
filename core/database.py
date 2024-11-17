dc2f8e0d62f79c985c070551725da79a86c4d6cc
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-11-17 19:28:59 +0530

    refactor: update db dependency

--- core/database.py
+++ core/database.py
@@ -11,7 +11,11 @@ Key Components:
 - Base: SQLAlchemy declarative base with naming conventions
 - BaseModal: Abstract base class with created_at and updated_at fields
 - set_tenant_schema: Utility for dynamic schema switching
+- get_db_context: Context manager for getting the session
 """
+
+from contextlib import asynccontextmanager
+from typing import Optional
 
 from sqlalchemy import MetaData, text
 from sqlalchemy.ext.asyncio import AsyncSession as SqlAlchemyAsyncSession
@@ -19,6 +23,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from config.settings import settings
+from core.constant import DB_PUBLIC_SCHEMA
 
 async_engine = create_async_engine(settings.DATABASE_URL)
 
@@ -39,11 +44,34 @@ naming_convention = {
 Base = declarative_base(metadata=MetaData(naming_convention=naming_convention))
 
 
-async def set_tenant_schema(session: SqlAlchemyAsyncSession, schema_name: str):
+async def set_tenant_schema(session: SqlAlchemyAsyncSession, *, schema: str):
     """Asynchronously checks if a given schema exists in the database session.
 
     Args:
         session (SqlAlchemyAsyncSession): The database session.
-        schema_name (str): The name of the schema to check.
+        schema (str): The name of the schema to check.
     """
-    await session.execute(text(f"SET search_path TO {schema_name}"))
+    await session.execute(text(f"SET search_path TO {schema}"))
+
+
+@asynccontextmanager
+async def get_db_context(schema: Optional[str] = DB_PUBLIC_SCHEMA):
+    """Get an SQLAlchemy database session.
+
+    Creates a new session using the AsyncSession class provided by database.py.
+    The session is closed after usage using a try/finally block.
+    Args:
+        schema (str): Tenant schema to use for the session. Default is DB_PUBLIC_SCHEMA.
+
+    Returns:
+        Session: The SQLAlchemy database session.
+    """
+    session = AsyncSession()
+    await set_tenant_schema(session, schema)
+    try:
+        yield session
+    except Exception:
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
---
+++ core/dependencies.py
@@ -0,0 +1,1 @@
+"""This module contains set of common dependencies that can be used over all apps."""
