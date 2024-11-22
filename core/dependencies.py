90c744eb35040e1ddfd7aa048955ed954db73e91
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-11-22 22:46:52 +0530

    refactor: add session dependency

--- core/dependencies.py
+++ core/dependencies.py
@@ -1,10 +1,12 @@
 """This module contains set of common dependencies that can be used over all apps."""
 
-from typing import Literal
+from typing import AsyncGenerator, Literal
 
 from fastapi import Query
 from sqlalchemy import asc, desc
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from core.database import get_db_context
 from core.enums import OrderByType
 
 
@@ -15,3 +17,22 @@ async def paginator_query_params(
 ):
     """Common dependency for the paginator query param."""
     return {"page": page, "size": size, "order_by": asc if order_by == OrderByType.ASC else desc}
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    """Asynchronous generator that yields a database session.
+
+    This function creates and yields an AsyncSession object within a database context.
+    It should be used as a dependency in FastAPI route functions to manage database connections.
+
+    Yields:
+        AsyncSession: An asynchronous SQLAlchemy session object.
+
+    Example:
+        @app.get("/items")
+        async def read_items(db: AsyncSession = Depends(get_db_session)):
+            # Use the db session here
+            ...
+    """
+    async with get_db_context() as session:
+        yield session
