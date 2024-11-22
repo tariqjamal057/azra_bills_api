bc1dfa83456e965dcd6fd8db5a22d7c00ba3d9ac
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-11-22 22:50:12 +0530

    feat: add pydantic class for saas_admin module

---
+++ azra_bills_api/admin/schemas/saas_admin.py
@@ -0,0 +1,70 @@
+"""This module contains the saas admin module schemas."""
+
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
+from core.utils import PhoneNumberValidator
+
+
+class CreateSAASAdmin(BaseModel, PhoneNumberValidator):
+    """Schema for creating a new SaaS admin.
+
+    Attributes:
+        username (str): The username of the admin. Must be between 3 and 50 characters long.
+        first_name (str): The first name of the admin. Must be between 3 and 50 characters long.
+        last_name (str): The last name of the admin. Must be between 3 and 50 characters long.
+        email (str): The email address of the admin. Must match the EMAIL_PATTERN.
+        phone_number (str): The phone number of the admin. Must match the PHONE_NUMBER_PATTERN.
+    """
+
+    username: str = Field(min_length=3, max_length=50, description="The username of the admin")
+    first_name: str = Field(min_length=3, max_length=50, description="The first name of the admin")
+    last_name: str = Field(min_length=3, max_length=50, description="The last name of the admin")
+    email: EmailStr = Field(max_length=100, description="The email address of the admin")
+    phone_number: str = Field(description="The phone number of the admin")
+
+
+class UpdateSAASAdmin(BaseModel, PhoneNumberValidator):
+    """Schema for creating a new SaaS admin.
+
+    Attributes:
+        username (str): The username of the admin. Must be between 3 and 50 characters long.
+        first_name (Optional[str]): The first name of the admin. Must be between 3 and 50
+        characters long.
+        last_name (Optional[str]): The last name of the admin. Must be between 3 and 50
+        characters long.
+        email (Optional[str]): The email address of the admin. Must match the EMAIL_PATTERN.
+        phone_number (Optional[str]): The phone number of the admin. Must match
+        the PHONE_NUMBER_PATTERN.
+    """
+
+    username: str = Field(min_length=3, max_length=50, description="The username of the admin")
+    first_name: Optional[str] = Field(
+        min_length=3, max_length=50, description="The first name of the admin"
+    )
+    last_name: Optional[str] = Field(
+        min_length=3, max_length=50, description="The last name of the admin"
+    )
+    email: Optional[EmailStr] = Field(max_length=100, description="The email address of the admin")
+    phone_number: Optional[str] = Field(description="The phone number of the admin")
+
+
+class ListSaaSAdmin(BaseModel):
+    """Schema for listing SaaS admin details.
+
+    Attributes:
+        id (int): The unique identifier of the admin.
+        first_name (str): The first name of the admin.
+        last_name (str): The last name of the admin.
+        email (str): The email address of the admin.
+        phone_number (str): The phone number of the admin.
+        is_active (bool): The active status of the admin account.
+    """
+
+    id: int = Field(description="The unique identifier of the admin")
+    first_name: str = Field(description="The first name of the admin")
+    last_name: str = Field(description="The last name of the admin")
+    email: str = Field(description="The email address of the admin")
+    phone_number: str = Field(description="The phone number of the admin")
+    is_active: bool = Field(description="The active status of the admin account")
